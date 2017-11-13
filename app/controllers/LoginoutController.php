cde661a7bb3983598d435f4dff464f7baa6785a3
simtaejin <stj53@nate.com>
2017-11-13 13:53:27 +0900

    did  - LoginoutController 추가 todo  - login페이지 생성(head, food 제외)

---
+++ app/controllers/LoginoutController.php
@@ -0,0 +1,25 @@
+<?php
+
+class LoginoutController extends ControllerBase
+{
+    public function indexAction()
+    {
+        $this->persistent->parameters = null;
+    }
+
+    /**
+     * 회원등록
+     */
+    public function RegisterAction()
+    {
+
+    }
+
+    /**
+     * 회원삭제
+     */
+    public function deRegisterAction()
+    {
+
+    }
+}
